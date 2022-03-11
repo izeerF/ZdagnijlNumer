583dcc102eb8ffd052cea66159f1ebda5c38fce5
izeerF <101408731+izeerF@users.noreply.github.com>
2022-03-11 15:00:21 +0100

    Dodaj pliki projektów.

---
+++ zdagnijliczbe/Program.cs
@@ -0,0 +1,29 @@
+﻿Random r = new Random();
+int rInt = r.Next(1, 11);
+int liczba;
+Console.WriteLine("Zgadnij liczbe:");
+while (true) {
+    try
+    {
+        liczba = int.Parse(Console.ReadLine());
+            
+    }
+    catch (System.FormatException)
+    {
+        Console.WriteLine("Musisz podac liczbe");
+        continue;
+    }
+    if (liczba == rInt)
+    {
+        Console.WriteLine("Gratulacje Zdadłes liczbe");
+        break;
+    }
+    else if(liczba > rInt)
+    {
+        Console.WriteLine("Twoja podana liczba liczba jest mniejsza");
+    }
+    else if (liczba < rInt)
+    {
+        Console.WriteLine("Twoja podana liczba liczba jest wieksza");
+    }
+}
